f75333810706adf408a3806471cdfa896aaca10b
jwl3562 <138647157+jwl3562@users.noreply.github.com>
2024-10-11 12:59:58 +0900

    Update and rename t.sh to run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --visible_devices=4,5,6,7 \
+python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py \
 --model deit_tiny_patch16_224  --server-model deit_base_patch16_224 --batch-size 1024 \
 --data-path /home/dataset/ImageNet --input-size 224 \
 --output_dir ./images/ --output_list 105,522,928,1200,9818,10697,14476 \
