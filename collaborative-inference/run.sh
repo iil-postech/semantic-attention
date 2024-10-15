c5edfa35f7cccad91e25578ba20d543f1681358d
jwl3562 <138647157+jwl3562@users.noreply.github.com>
2024-10-15 20:23:26 +0900

    Update run.sh

--- collaborative-inference/run.sh
+++ collaborative-inference/run.sh
@@ -3,4 +3,4 @@ python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py \
 --data-path /home/dataset/ImageNet --input-size 224 \
 --output_dir ./images/ --output_list 105,522,928,1200,9818,10697,14476 \
 --attention_mode mean --masking_mode attention_sum_threshold --masking_th 0.97 \
---uncer_mode min_entropy --uncer_th 1.0
+--uncer_mode min_entropy --uncer_th 0.8
